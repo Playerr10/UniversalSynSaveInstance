913c3df23bb3e6035b9ee5ff343e2fc966d0ca22
Playerr10 <157866467+Playerr10@users.noreply.github.com>
2025-05-23 15:30:57 -0500

    Delete saveinstance.lua

--- saveinstance.lua
+++
@@ -1,8 +0,0 @@
-local Params = {
-	RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
-	SSI = "saveinstance",
-}
-local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".lua", true), Params.SSI)()
-return function(Options)
-	synsaveinstance(Options)
-end
